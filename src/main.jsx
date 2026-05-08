d771ae51ca5a28a539cdd9c7e3c169246699ab2b
maysmethodlab-create <maysmethodlab@gmail.com>
2026-05-08 14:08:41 -0500

    Performance: self-host Oswald + Work Sans via @fontsource (eliminate Google Fonts round-trip)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/oswald/400.css";
+import "@fontsource/oswald/500.css";
+import "@fontsource/oswald/600.css";
+import "@fontsource/oswald/700.css";
+import "@fontsource/work-sans/400.css";
+import "@fontsource/work-sans/500.css";
+import "@fontsource/work-sans/600.css";
+import "@fontsource/work-sans/700.css";
+import "@fontsource/work-sans/400-italic.css";
 import Map from "./components/Map.jsx";
 
 createRoot(document.getElementById("root")).render(<Map />);
